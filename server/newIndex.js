14ffad87ebf2e5e88fddc01202aa6da2ab73a611
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-03 21:17:27 -1000

    get req working with connection to db

--- server/newIndex.js
+++ server/newIndex.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { getReviews } = require('../database/newIndex');
 
 const app = express();
 const port = 3003;
@@ -11,11 +12,17 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/api/products/:id/reviews', (req, res) => {
   // query function to get all reviews for one product
-})
+  // on page load, sort defaults to 'Most relevant'
+  const { sort, stars } = req.query;
+  const { id } = req.params;
+  getReviews(id, stars, sort, (results) => {
+    console.log(results.rows);
+  });
+});
 
-app.patch('/api/products/:id/reviews/:reviewid', (req, res) => {
-  // query function to update review
-})
+app.patch('/api/reviews/:reviewid', (req, res) => {
+  // query function to update review (upvotes/downvotes)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
